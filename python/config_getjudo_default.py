8cda1a28d656ca8649c7fa3497ef84e8a57917fa
Martin Patzel <martin@patzel.de>
2023-02-12 19:37:48 +0100

    my config

--- python/config_getjudo_default.py
+++ python/config_getjudo_default.py
@@ -6,15 +6,15 @@ JUDO_USER = "myjudousername"
 JUDO_PASSWORD = "myjudopassword"
 
 #MQTT Config
-BROKER = "192.168.1.2"              #Broker IP
-USE_MQTT_AUTH = True                #Set true, if user/pw authentification on broker, set false, if using anonymous login
+BROKER = "192.168.1.35"             #Broker IP
+USE_MQTT_AUTH = False               #Set true, if user/pw authentification on broker, set false, if using anonymous login
 MQTTUSER = "mqttuser"               #only required if USE_MQTT_AUTH = True
 MQTTPASSWD = "mosquitto"            #only required if USE_MQTT_AUTH = True
 PORT = 1883                         #MQTT PORT, 1883 default standard
 
 #General Config
-LOCATION = "my_location"            #Location of Judo device
-NAME = "Judo_isoftsaveplus"         #Name of Judo device
+LOCATION = "Balzheim"            #Location of Judo device
+NAME = "Judo-isoft"         #Name of Judo device
 MANUFACTURER = "ShapeLabs.de"       #CC BY-NC-SA 4.0
 SW_VERSION = "2.0"
 STATE_UPDATE_INTERVAL = 20          #Update interval in seconds
@@ -32,7 +32,7 @@ LIMIT_EXTRACTION_TIME = 60          #can setup to max 600min
 LIMIT_MAX_WATERFLOW = 3000          #can setup to max 5000L/h 
 LIMIT_EXTRACTION_QUANTITY = 500     #can setup to max 3000L
 
-RUN_IN_APPDAEMON = True                    #'True' , if running in Appdaemon docker directly in HA, on generic Linux platform set flag to 'False'
+RUN_IN_APPDAEMON = False            #'True' , if running in Appdaemon docker directly in HA, on generic Linux platform set flag to 'False'
 # for Appdaemon the whole path is required "/config/appdaemon/apps/main/temp_getjudo.pkl", otherwise "temp_getjudo.pkl"
 TEMP_FILE = "/config/appdaemon/apps/main/temp_getjudo.pkl"
 #TEMP_FILE = "temp_getjudo.pkl"
